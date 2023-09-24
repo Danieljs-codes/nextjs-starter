74ef147761e8f81784dbdd3e86d11daf3fe45505
Olamide <onabiyiolamide13@gmail.com>
2023-09-24 20:33:00 +0100

    Fix line endings

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="ola bg-black px-2 py-3 font-bold text-white">Home</div>
+  );
+};
+
+export default Home;
---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable unicorn/prefer-module */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx', 'cn', 'cva'],
+};
